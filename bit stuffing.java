3984f69f44d97aafdea6296b7f5c352e55d1205e
Vineet Vemula <vindare@gmail.com>
2017-11-04 16:39:56 +0530

    Update bit stuffing.java

--- bit stuffing.java
+++ bit stuffing.java
@@ -11,7 +11,9 @@ class bs
         String ip = sc.next();
         char a[] =new char[50];
         ip.getChars(0,ip.length(),a,0);
-        int i,c=0,j;
+        int i,c=0,j,m;
+        char res[]=new char[50];
+        char flag[]= new char[]{'0','1','1','1','1','1','1','0'};
         for(i=0;i<n;i++)
         {
             if(a[i]=='1')
@@ -32,9 +34,22 @@ class bs
             }
         }
         System.out.println(" Message will be sent as");
-        for(i=0;i<n;i++)
+        m=n+16;
+        for(i=0;i<8;i++)
         {
-            System.out.print(a[i]+" ");
+          res[i]= flag[i];
+        }
+        for(j=8,i=0;i<n;j++,i++)
+        {
+          res[j]=a[i];
+        }
+        for(i=n+8,j=0;j<8;i++,j++)
+        {
+          res[i]=flag[j];
+        }
+        for(i=0;i<n+16;i++)
+        {
+            System.out.print(res[i]+" ");
         }
         c=0;
         System.out.println("\n Message recieved and decoded as");
@@ -64,3 +79,17 @@ class bs
         System.out.println();
     }
 }
+
+
+
+OUTPUT
+
+Enter the length of input String
+7
+Enter the input String
+1111110
+ Message will be sent as
+0 1 1 1 1 1 1 0 1 1 1 1 1 0 1 0 0 1 1 1 1 1 1 0 
+ Message recieved and decoded as
+1 1 1 1 1 1 0 
+
